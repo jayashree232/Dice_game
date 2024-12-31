580531496e55fa3bad6cfb578df9d7678e66997a
jayashree232 <68339364+jayashree232@users.noreply.github.com>
2024-12-31 23:06:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ document.getElementById("reloadButton").addEventListener("click", handleReload);
 
 function handleReload() {
   var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-  var randomImage1Source = "images/dice" + randomNumber1 + ".png";
+  var randomImage1Source = "dice" + randomNumber1 + ".png";
   var image1 = document
     .querySelectorAll("img")[0]
     .setAttribute("src", randomImage1Source);
 
   var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-  var randomImage2Source = "images/dice" + randomNumber2 + ".png";
+  var randomImage2Source = "dice" + randomNumber2 + ".png";
   var image2 = document
     .querySelectorAll("img")[1]
     .setAttribute("src", randomImage2Source);
